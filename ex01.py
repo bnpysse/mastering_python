962681ac204c4ba98e3b9d10a6923a49a24feada
bnpysse <bnpysse@aliyun.com>
2025-05-21 08:23:00 +0800

    01.init

--- ex01.py
+++ ex01.py
@@ -1,18 +1,10 @@
-city = "London"
-def withoutGlobal():
-    city = "Paris"
-def withGlobal():
-    """ 
-    This function changes the global variable city to Paris.
-    """
-    global city
-    city = "Paris"
+print(eval("5+2*3-1/2"))
+print(eval("pow(2,4)-5+max(10,5)"))
 
-withoutGlobal()
-print(city)
-
-withGlobal()
-print(city)
-
-print("__var".isidentifier())
-print(withGlobal.__doc__)
+var = 'computer'
+print(id(var))
+del(var)
+try:
+    print(var)
+except NameError:
+    print("NameError: name 'var' is not defined")
